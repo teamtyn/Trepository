3fdae9d75d5128000be137900ea5a6f6da5b06ba
ckucera3 <claytonkucera@gmail.com>
2014-09-12 22:33:17 -0400

    Revert "Revert "NetBeans Project""
    This reverts commit 4d8e0539d8762d61d2488af819cbbcb120604358.

---
+++ JavaFXML[SkillInterfaceTest][CS2340]/src/javafxml/skillinterfacetest/cs2340/MenuController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javafxml.skillinterfacetest.cs2340;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MenuController implements Initializable, ControlledScreen {
+    
+    @FXML
+    private ScreensController parentController;
+    
+    @FXML
+    private void startButtonAction(ActionEvent event) {
+        parentController.setScreen("SkillSetup");
+    }
+    
+    @FXML
+    private void exitButtonAction(ActionEvent event) {
+        System.exit(1);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void setScreenParent(ScreensController parentController) {
+        this.parentController = parentController;
+    }
+}
